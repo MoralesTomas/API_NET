72edcc710041e9799a8feb7c83007de57ce77d7c
AlejoMora991014 <alejandroester1910@gmail.com>
2022-12-06 16:13:55 -0600

    inyeccion de dependencias al builder de la API

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using API_NET.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +8,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//esto es para la inyeccion de dependencias
+//especificamente este es para pasar la interfaz y luego la clase esto limita si necesitamos mandar parametros a la clase
 //builder.Services.AddScoped<IHelloWorldService, HelloWorldService>();
+
+//este es para la inyeccion de dependencias pero haciendo una instancia de manera que si podemos mandar parametros
 builder.Services.AddScoped<IHelloWorldService>(p=> new HelloWorldService());
+
+
+builder.Services.AddScoped<ICategoriaService, CategoriaService>();
+builder.Services.AddScoped<ITareasService, TareasService>();
 
 
 var app = builder.Build();
